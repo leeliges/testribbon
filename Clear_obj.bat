94e01e2907a1f0b3bf4e610c3b6f422eb058302b
leeliges <54797683+leeliges@users.noreply.github.com>
2021-08-16 13:29:49 +0900

    Add files via upload

---
+++ Clear_obj.bat
@@ -0,0 +1,16 @@
+@echo off
+del *.htm /s /q
+del *.ilk /s /q
+del *.exp /s /q
+del *.pdb /s /q
+del *.obj /s /q
+del *.pch /s /q
+del *.dep /s /q
+del *.ncb /s /q
+del *.aps /s /q
+del *.idb /s /q
+del *.ipch /s /q
+del *.sdf /s /q
+del *.d /s /q
+del *.sbr /s /q
+del *.tlog /s /q
